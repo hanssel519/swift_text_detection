b4d64b4c7724007a3f4a9cfab18e2f505f2cbd97
hanssel519 <winnie8751923@gmail.com>
2024-07-31 19:30:02 +0800

    Initial Commit

---
+++ text_recognition_ver1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  text_recognition_ver1
+//
+//  Created by winniechu on 2024/7/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ text_recognition_ver1/text_recognition_ver1App.swift
@@ -0,0 +1,17 @@
+//
+//  text_recognition_ver1App.swift
+//  text_recognition_ver1
+//
+//  Created by winniechu on 2024/7/31.
+//
+
+import SwiftUI
+
+@main
+struct text_recognition_ver1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
